e2f8ab21fa6cd5d06a4fc1625ca09387324f8c97
Zz1p <a18357359414@icloud.com>
2020-03-27 15:56:56 +0800

    1

--- music-player-back/app.js
+++ music-player-back/app.js
@@ -1,3 +1,25 @@
-import koa from 'koa'
-conosle.log(1);
-console.log(2);
+import Koa from 'koa';
+import Router from 'koa-router';
+import source from 'koa-static';
+import bodyParser from 'koa-bodyparser';
+// post ctx.reqyest.fields get ctx.query
+const app = new Koa();
+app.use(bodyParser());
+const router = new Router();
+const list = new Router({
+    prefix: '/list'
+});
+
+list.get('/:index', async ctx => {
+    ctx.body = ctx.params.index;
+});
+
+router.post('/login', async ctx => {
+    ctx.body = 'login'
+});
+
+
+app.use(source('./static'));
+app.use(list.routes());
+app.use(router.routes());
+app.listen(3000);
