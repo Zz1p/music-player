684ecf1edff0fa87a6a2649ff9db700b50d1218e
Zz1p <a18357359414@icloud.com>
2020-03-31 10:43:29 +0800

    'addToken'

--- music-player-back/app.js
+++ music-player-back/app.js
@@ -1,33 +1,11 @@
-import Koa from 'koa';
 import source from 'koa-static';
-import bodyParser from 'koa-bodyparser';
-import router from './routes.js';
-import cors from 'koa2-cors';
-import jwt from 'koa-jwt';
+import {router, app} from './routes.js';
 
-const app = new Koa();
 // post ctx.request.fields get ctx.query
 
-app.use(bodyParser());
-app.use(jwt({
-    secret: 'music_player_token'
-}).unless({
-    path: [/\/login/]
-}));
-app.use((ctx, next) => {
-    return next().catch((err) => {
-        if (err.status === 401) {
-            ctx.status = 401;
-            ctx.body = {
-                ok: false,
-                msg: err.originalError ? err.originalError.message : err.message
-            }
-        } else {
-            throw err;
-        }
-    });
-});
-app.use(cors());
+
+
+
 app.use(source('./static'));
 app.use(router.routes());
 app.use(router.allowedMethods());
--- music-player-back/routes.js
+++ music-player-back/routes.js
@@ -1,15 +1,47 @@
 import Router from "koa-router";
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import cors from 'koa2-cors';
+import koaJwt from 'koa-jwt';
+import jwt from 'jsonwebtoken';
 
+const app = new Koa();
 const router = new Router();
+const jwtSecret = 'music_player_token';
 
+app.use(bodyParser());
+app.use(koaJwt({
+    secret: jwtSecret
+}).unless({
+    path: [/\/login/]
+}));
+app.use(cors());
+app.use((ctx, next) => {
+    return next().catch((err) => {
+        if (err.status === 401) {
+            ctx.status = 401;
+            ctx.body = 'Protected resource, use Authorization header to get access\n';
+        } else {
+            throw err;
+        }
+    });
+});
 router.get('/api/list', async ctx => {
     ctx.body = ctx.query;
 });
 
 router.post('/api/login', async ctx => {
+    const user = ctx.request.body;
     ctx.body = {
         validUser: true,
-        token: jwt.sign({username: ctx.request.fields.username, password: ctx.request.fields.password}, )
+        username: user.username,
+        token: jwt.sign({username: user.username, password: user.password}, jwtSecret)
     }
 });
-export default router;
+
+export {
+    router,
+    app
+};
+
+// https://www.jianshu.com/p/794ba23e68ad
