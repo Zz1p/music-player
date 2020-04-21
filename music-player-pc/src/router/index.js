7670ddb5ca2114b79fa430e88698fb25a656a8b6
zhufengping <zhufengping@edusoho.com>
2020-04-21 20:25:08 +0800

    new0function

--- music-player-pc/src/router/index.js
+++ music-player-pc/src/router/index.js
@@ -1,29 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store/index.js'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/user/Login.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "register" */ '../views/user/Register.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to, from , next) => {
+  console.log(store.state.isAuthenticated);
+  if (to.name !== 'Login' && !store.state.isAuthenticated) next({ name: 'Login' })
+  else next()
+});
 
 export default router
--- music-player-pc/src/views/user/Register.vue
+++ music-player-pc/src/views/user/Register.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <div></div>
 </template>
 
 <script>
@@ -8,6 +8,6 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 
 </style>
