bccae28b4b44b2bd1dbd0e75119ee5c171e0135e
zhufengping <zhufengping@edusoho.com>
2020-04-23 22:08:33 +0800

    newpage

--- music-player-pc/src/App.vue
+++ music-player-pc/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
+    <my-header></my-header>
     <router-view/>
   </div>
 </template>
-
+<script>
+  import myHeader from './components/Header.vue';
+  export default {
+    components: {
+      myHeader
+    }
+  }
+</script>
 <style lang="less">
-  @import 'styles/Global.less';
-
   body {
     margin: 0;
   }
@@ -17,20 +21,6 @@
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
   }
 
-  #nav {
-    padding: 30px;
-  }
-
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
 </style>
--- music-player-pc/src/components/Header.vue
+++ music-player-pc/src/components/Header.vue
@@ -1,13 +1,53 @@
 <template>
-  $END$
+  <div id="nav">
+    <el-menu :default-active="path"
+             class="el-menu-demo"
+             router
+             mode="horizontal">
+      <el-menu-item>音乐播放器后台管理系统</el-menu-item>
+      <el-menu-item class="right" @click="logout">退出登录</el-menu-item>
+      <el-menu-item class="right" index="/login">登录</el-menu-item>
+      <el-menu-item class="right" index="/register">注册</el-menu-item>
+      <el-menu-item class="right" index="/">推荐管理</el-menu-item>
+      <el-menu-item class="right" index="/">歌单管理</el-menu-item>
+      <el-menu-item class="right" index="/">歌曲管理</el-menu-item>
+    </el-menu>
+    <div class="line"></div>
+  </div>
+
 </template>
 
 <script>
   export default {
-    name: "Header"
+    name: "Header",
+    data() {
+      return {
+        path: '',
+      }
+    },
+    watch: {
+      '$route': {
+        handler() {
+          this.onRouteChanged();
+        },
+        immediate: true
+      }
+    },
+    methods: {
+      onRouteChanged() {
+        this.path = this.$route.path
+      },
+      logout() {
+        console.log('logout')
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  #nav {
+    .right {
+      float: right;
+    }
+  }
 </style>
